dbb3a50faba2b5c4cf7e658b7b0f85b68e29bf92
Jan Zacharzewski <janzacharzewski4@gmail.com>
2019-09-13 17:03:20 +0200

    JSONRESPONSE

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -3,6 +3,8 @@ package pl.coderslab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.domain.Book;
+import pl.coderslab.domain.JsonResponse;
+import pl.coderslab.exception.ServiceExeption;
 import pl.coderslab.service.BookService;
 
 import java.util.List;
@@ -34,12 +36,33 @@ public class BookController {
     public List<Book> findAll() {
         return service.findAll();
     }
+
     @RequestMapping("/{id}")
     public Book findById(@PathVariable long id) {
         return service.findById(id);
     }
+
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public Book create(@RequestBody Book book) {
         return service.create(book);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public JsonResponse delete(@PathVariable long id) {
+        try {
+            if (service.delete(service.findById(id))) {
+                return new JsonResponse(200, "Successfully delete book:" + id);
+            } else {
+                return new JsonResponse(500, "Cannot remove book");
+            }
+        } catch (ServiceExeption e) {
+            return new JsonResponse(500, e.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Book update(@PathVariable long id, @RequestBody Book book) {
+        book.setId(id);
+        return service.update(book);
     }
 }
